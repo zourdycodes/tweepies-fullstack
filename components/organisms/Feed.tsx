c3cdd56bb1468c2df4e8a0345c5c982594df0d04
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-21 15:14:08 +0000

    📱 design: working on the feeds section of the app
    add some input and button onto it

--- components/organisms/Feed.tsx
+++ components/organisms/Feed.tsx
@@ -1,9 +1,13 @@
+import { SparklesIcon } from '@heroicons/react/outline';
 import React from 'react';
 
 export const Feed: React.FC = () => {
   return (
-    <div>
-      <h1>feeds</h1>
+    <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
+        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+        <SparklesIcon className="h-5 text-white" />
+      </div>
     </div>
   );
 };
--- components/organisms/Widgets.tsx
+++ components/organisms/Widgets.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Widgets: React.FC = () => {
   return (
     <div>
-      <h1>Widgets</h1>
+      <h1></h1>
     </div>
   );
 };
