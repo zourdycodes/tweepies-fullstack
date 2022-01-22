c6dfa1245a22cd13798e03f3d8860e9a0d024bd5
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-22 10:26:10 +0000

    feix: forgot to call the API request for trending results

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,53 @@ import { Feed } from '../components/organisms/Feed';
 import { Sidebar } from '../components/molecules/Sidebar';
 import { Widgets } from '../components/organisms/Widgets';
 import { Layout } from '../components/templates/Layout';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  PreviewData,
+} from 'next';
+import { FollowResult, TrendingResult } from '@/types';
+import { ParsedUrlQuery } from 'querystring';
 
-const IndexPage: React.FC = () => {
+type Props = {
+  trendingResults: TrendingResult[];
+  followResults: FollowResult[];
+};
+
+const IndexPage: React.FC<Props> = ({ trendingResults, followResults }) => {
   return (
     <Layout title="Home / Tweepies">
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <Feed />
-        <Widgets />
+        <Widgets
+          followResult={followResults}
+          trendingResult={trendingResults}
+        />
       </main>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  );
+
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  );
+
+  console.log(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+    },
+  };
 };
 
 export default IndexPage;
